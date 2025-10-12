987930514452e94f147296577a0bfd10a89b9a13
Michael Friendly <friendly@yorku.ca>
2025-10-12 17:11:35 -0400

    add ggChernoff example

--- examples/faces.R
+++ examples/faces.R
@@ -15,3 +15,22 @@ set.seed(42)
 sampled_rows <- sample(1:nrow(penguins), size = 20)
 
 PlotFaces(penguins[sampled_rows, 3:6])
+
+## ggChernoff package
+## 
+library(ggChernoff)
+library(ggplot2)
+
+p <- ggplot(iris) +
+aes(Sepal.Width, Sepal.Length, fill = Species, brow = Sepal.Length) +
+geom_chernoff()
+p
+p + scale_brow_continuous(midpoint = min)
+p + scale_brow_continuous(range = c(-.5, 2))
+
+plt <- ggplot(penguins[sampled_rows,],
+              aes(x = body_mass, y = flipper_len,
+                  fill = species, nose = bill_len)) +
+  geom_chernoff()
+                          
+
